1ab517784e795e5a668e3b03c53db796bfcb9778
Amanda L <alarson672@gmail.com>
2021-05-18 21:45:51 -0700

    trying to fix errors

--- api/app/config/db.config.js
+++ api/app/config/db.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // url: `mongodb+srv://nomaan:${process.env.MONGODB_PASSWORD}@timesheetcluster.kcdxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-     url: `mongodb+srv://nomaan:pa55word@timesheetcluster.kcdxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    url: `mongodb+srv://nomaan:${process.env.MONGODB_PASSWORD}@timesheetcluster.kcdxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 };
